86bba537ad90e32d5964721a7c2139e3b420fb1b
Denise Buttigieg <denise.buttigieg.14@um.edu.mt>
2018-01-06 15:50:35 +0100

    added constructor for Message class

--- src/main/java/com/cps3222/Message.java
+++ src/main/java/com/cps3222/Message.java
@@ -7,8 +7,16 @@ public class Message {
     public String sourceAgentId;
     public String targetAgentId;
     public long timestamp;
+    public String content;
 
     public Message(){
 
+    }
+
+    public Message(String sourceAgentId, String targetAgentId, String content, long timestamp){
+        this.sourceAgentId = sourceAgentId;
+        this.targetAgentId = targetAgentId;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 }
--- src/test/java/com/cps3222/MailboxTest.java
+++ src/test/java/com/cps3222/MailboxTest.java
@@ -3,12 +3,17 @@ package com.cps3222;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by denise on 02/01/2018.
  */
 public class MailboxTest {
     @Before
     public void setUp() throws Exception {
+        Queue<Message> mailboxQueue = new LinkedList<Message>();
+
 
     }
 
