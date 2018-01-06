3e12239f57d34f84de14e3835833b29dd514c2ec
MUDELL\Raoul <MUDELL\raoulfenech@gmail.com>
2018-01-06 15:52:28 +0100

    Made tests for Mailbox methods

--- src/test/java/com/cps3222/MailboxTest.java
+++ src/test/java/com/cps3222/MailboxTest.java
@@ -6,25 +6,51 @@ import org.junit.Test;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by denise on 02/01/2018.
  */
 public class MailboxTest {
+
+    private Queue<Message> mockMailbox = new LinkedList<Message>();
+    private Mailbox m;
+
     @Before
     public void setUp() throws Exception {
-        Queue<Message> mailboxQueue = new LinkedList<Message>();
 
-
+        m = new Mailbox();
+        mockMailbox.add(new Message());
     }
 
     @Test
-    public void consumeNextMessage() throws Exception {
+    public void testConsumeMessage_emptyMailbox() throws Exception {
 
+        //making the queue empty
+        mockMailbox.remove();
+
+        assertEquals(null, m.consumeNextMessage(mockMailbox));
     }
 
     @Test
-    public void hasMessages() throws Exception {
+    public void testConsumeMessage() throws Exception {
 
+        assertEquals(mockMailbox.peek(), m.consumeNextMessage(mockMailbox));
+    }
+
+    @Test
+    public void testHasMessages_emptyMailbox() throws Exception {
+
+        //making the queue empty
+        mockMailbox.remove();
+
+        assertEquals(false, m.hasMessages(mockMailbox));
+    }
+
+    @Test
+    public void testHasMessages() throws Exception {
+
+        assertEquals(true, m.hasMessages(mockMailbox));
     }
 
 }
